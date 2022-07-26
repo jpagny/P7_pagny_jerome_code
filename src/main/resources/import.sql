489301c5dd117bd206e70e4072865c0054a88c19
Jérôme Pagny <pagny.jerome@gmail.com>
2022-07-26 09:12:17 +0200

    Add webSecurity + fix template curvepoint list

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
 insert into users(id,fullname, username, password, role) values(1,"Administrator", "admin", "$2a$12$mWjZIrVAxn4VgynPO54qyuD.UKE6ZiGqw0k8ux8hL9eXqzlOTf9Ta", "ADMIN");
 insert into users(id,fullname, username, password, role) values(2,"User", "user", "$2a$12$rbDoeehRlwObv3EX2hDDA.sjkbW9HmLFkyL4TOPltuk/Izk7uAlTK", "USER");
+
+INSERT INTO `curvepoint` (`id`, `as_of_date`, `creation_date`, `curve_id`, `term`, `value`) VALUES ('1', '2022-07-26 09:00:55', '2022-07-26 09:00:55', '1', '30', '50');
+INSERT INTO `curvepoint` (`id`, `as_of_date`, `creation_date`, `curve_id`, `term`, `value`) VALUES ('2', '2022-07-29 09:00:55', '2022-07-04 09:09:50', '2', '10', '100');
