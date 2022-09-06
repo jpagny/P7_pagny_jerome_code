0f90f8c28b34b50d20392923b66797bb55ada198
Jérôme Pagny <pagny.jerome@gmail.com>
2022-09-06 11:20:27 +0200

    Add new IT : HomeControllerTestIT

--- src/test/java/com/nnk/springboot/controller/HomeControllerTestIT.java
+++ src/test/java/com/nnk/springboot/controller/HomeControllerTestIT.java
@@ -13,21 +13,19 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @SpringBootTest(classes = Application.class)
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration
 @AutoConfigureMockMvc(addFilters = false)
-@Transactional
-public class LoginControllerTestIT {
+public class HomeControllerTestIT {
 
     private MockMvc mockMvc;
 
@@ -43,20 +41,41 @@ public class LoginControllerTestIT {
     }
 
     @Test
-    @DisplayName("Should be returned 200 when get page login")
-    public void should_beReturned200_when_getPageLogin() throws Exception {
-        this.mockMvc.perform(get("/login")).andDo(print()).andExpect(status().isOk());
+    @DisplayName("Should be returned 200 when get page home")
+    public void should_beReturned200_when_getPageHome() throws Exception {
+        this.mockMvc.perform(get("/home"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
     }
 
     @Test
-    @DisplayName("Should be redirect to login page when user is not authenticated")
-    public void should_beRedirectToLoginPage_when_userIsNotAuthenticated() throws Exception {
-        this.mockMvc.perform(get("/bidList/list"))
-                .andDo(print())
+    @DisplayName("Should be redirect to home page when get page /")
+    public void should_beReturned200_when_getPageRacine() throws Exception {
+        mockMvc.perform(get("/"))
                 .andExpect(status().is3xxRedirection())
-                .andExpect(header().string("Location", "http://localhost/login"));
+                .andExpect(view().name("redirect:/home"))
+                .andReturn();
     }
 
+    @Test
+    @DisplayName("Should be redirect to bidList/list page when get page /admin/home")
+    @WithUserDetails("admin")
+    public void should_beRedirectToBidListListPage_when_getPageAdminHome() throws Exception {
+        mockMvc.perform(get("/admin/home"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/admin/bidList/list"))
+                .andReturn();
+    }
 
+    @Test
+    @DisplayName("Should be homeUser page when get page /user/home")
+    @WithUserDetails()
+    public void should_beHomeUserPage_when_getPageUserHome() throws Exception {
+        mockMvc.perform(get("/user/home"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("homeUser"))
+                .andReturn();
+    }
 
 }
