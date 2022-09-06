40eda7998bc76f428151e2a4fe811c019f10bcf4
Jérôme Pagny <pagny.jerome@gmail.com>
2022-09-06 09:43:34 +0200

    Change code for resource not found error : 404 -> 400

--- src/main/java/com/nnk/springboot/controller/web/MyErrorController.java
+++ src/main/java/com/nnk/springboot/controller/web/MyErrorController.java
@@ -1,30 +1,17 @@
 package com.nnk.springboot.controller.web;
 
 import com.nnk.springboot.exception.ResourceNotFoundException;
-import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class MyErrorController {
-
-    // For UI Pages
     @ExceptionHandler(ResourceNotFoundException.class)
     public String resourceNotFoundException(ResourceNotFoundException ex) {
-        return "error/404.html";
+        return "error/400.html";
     }
 
-    // For REST APIs
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<?> illegalArgumentException(IllegalArgumentException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
-    }
 
 }
